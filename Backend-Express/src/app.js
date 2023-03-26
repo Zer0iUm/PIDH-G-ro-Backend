8c9ab2195a8006efac37dd746180778720515727
Julio Nunes <julio.nt753@gmail.com>
2023-03-26 19:56:58 -0300

    teste1

--- Backend-Express/src/app.js
+++ Backend-Express/src/app.js
@@ -1,27 +1,41 @@
-const routes = require('./routes/index')
-const path = require("path")
-const express = require('express')
+const routes = require('./routes/index');
+const path = require('path');
+const express = require('express');
+const bodyparser = require('body-parser');
+const session = require('express-session');
+const { v4: uuidv4 } = require('uuid');
 //const methodOverride = require('method-override') // métodos PUT e DELETE
-const app = express()
+const app = express();
 // captura na forma de objeto literal tudo o que vem de um formulário
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
 // converte as informações em formato JSON
-app.use(express.json())
+app.use(express.json());
 // métodos PUT e DELETE
 //app.use(methodOverride('_method'))
 
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
+
 // instanciando como view engine
-app.set("view engine", "ejs")
+app.set('view engine', 'ejs');
 // instanciando pasta views
-app.set("views", path.resolve("src", "views"))
+app.set('views', path.resolve('src', 'views'));
 // liberando acesso a pasta public
-app.use(express.static(path.resolve("public")))
+app.use(express.static(path.resolve('public')));
+
+app.use(
+	session({
+		secret: uuidv4(), //  '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
+		resave: false,
+		saveUninitialized: true,
+	})
+);
 
 /**
  * Rotas
  */
-app.use(routes)
+app.use(routes);
 
 app.listen(3000, () => {
-  console.log('Servidor Rodando!')
-})
+	console.log('Servidor Rodando!');
+});
