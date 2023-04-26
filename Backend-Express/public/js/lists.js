c77d9df1f22531bb67828ca4e43dd9e44b084369
Vinícius Rotondo <vinirs.trade@gmail.com>
2023-04-26 20:10:10 -0300

    Adicionando filtros(incompleto)

--- Backend-Express/public/js/lists.js
+++ Backend-Express/public/js/lists.js
@@ -5,6 +5,32 @@ window.addEventListener('load', function() {
       btSacola[i].addEventListener('click', function() {
         alert('Item adicionado com sucesso!');
       }); 
- 
     }
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]')
+
+    checkboxes.forEach(checkbox => {
+      checkbox.addEventListener('change', filterItems)
+    })
+
+    const filterItems = () => {
+      const items = document.querySelectorAll('.item')
+      const filters = document.querySelectorAll('input[type="checkbox"]:checked');
+
+      items.forEach(item => {
+        let shouldShow = true;
+
+        filters.forEach(filter => {
+          if(!item.classList.contains(filter.value)){
+            shouldShow = false;
+          }
+        })
+        if(shouldShow){
+          item.style.display = 'block'
+        }else{
+          item.style.display = 'none'
+        }
+      })
+    }
+
+
   });
